a4976a181db87e147b6d697e993ba591a78b4f27
g-maiyazu <emujei6123@yahoo.co.jp>
2020-03-06 22:24:16 +0900

    Add user model validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  it '有効なファクトリを持つこと' do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
+  it 'メールアドレス、パスワードがあれば有効であること' do
+    user = User.new(
+      email: 'tester@example.com',
+      password: 'password'
+    )
+    expect(user).to be_valid
+  end
+
+  it 'メールアドレスがなければ無効であること' do
+    @user.email = nil
+    @user.valid?
+    expect(@user.errors).to be_added(:email, :blank)
+  end
+
+  it 'パスワードがなければ無効であること' do
+    @user.password = @user.password_confirmation = ' ' * 6
+    @user.valid?
+    expect(@user.errors).to be_added(:password, :blank)
+  end
+
+  describe '値の一意確認' do
+    it '重複したメールアドレスなら無効であること' do
+      user = FactoryBot.create(:user, email: 'foobar@example.com')
+      duplicate_user = FactoryBot.build(:user, email: 'foobar@example.com')
+      duplicate_user.valid?
+      expect(duplicate_user.errors).to be_added(:email, :taken, value: 'foobar@example.com')
+    end
+  end
+
+  describe '文字数の検証' do
+    it 'メールアドレスが255文字以内なら有効であること' do
+      @user.email = 'a' * 200 + '@example.com'
+      expect(@user).to be_valid
+    end
+
+    it 'メールアドレスが255文字を越えるなら無効であること' do
+      @user.email = 'a' * 255 + '@example.com'
+      @user.valid?
+      expect(@user.errors).to be_added(:email, :too_long, count: 255)
+    end
+
+    it 'パスワードが6文字未満なら無効であること' do
+      @user.password = @user.password_confirmation = 'a' * 5
+      @user.valid?
+      expect(@user.errors).to be_added(:password, :too_short, count: 6)
+    end
+
+    it 'パスワードが6文字以上なら有効であること' do
+      @user.password = @user.password_confirmation = 'a' * 6
+      expect(@user).to be_valid
+    end
+  end
 end
