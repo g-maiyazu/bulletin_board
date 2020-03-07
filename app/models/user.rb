1bfeea825300fad6662fbfa2f0f9c6f06015f30d
g-maiyazu <emujei6123@yahoo.co.jp>
2020-03-07 21:35:17 +0900

    Add account setting
    アカウント設定の追加(アカウント編集機能の追加)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  mount_uploader :avatar, AvatarUploader
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email,         length: { maximum: 255 },
