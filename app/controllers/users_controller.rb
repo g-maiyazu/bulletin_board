2a9ddda2fc58c696c91f5ecb11cc731a89a5aeda
g-maiyazu <emujei6123@yahoo.co.jp>
2020-03-11 23:40:02 +0900

    Modify layouts
    静的ページ、ユーザーページ、投稿ページのレイアウトを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.page(params[:page]).per(6)
+    @posts = @user.posts.page(params[:page]).per(PER)
   end
 end
